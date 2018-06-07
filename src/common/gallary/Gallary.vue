4fba1eb6d6698eccabe6039a77c0f0027b76bbb7
llb <277095228@qq.com>
2018-06-07 16:18:06 +0800

    add common

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,11 @@
+<template>
+	<div>sdafjsadlkf</div>
+</template>
+<script>
+	export default {
+		name:  'CommonGallary'
+	}
+</script>
+<style>
+	
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/pages/home/Home'
 import City from '@/pages/city/City'
+import Detail from '@/pages/detail/Detail'
 
 Vue.use(Router)
 
@@ -12,9 +13,13 @@ export default new Router({
       name: 'Home',
       component: Home
     }, {
-    	path: '/City',
+    	path: '/city',
       name: 'City',
       component: City
+    }, {
+      path: '/detail/:id',
+      name: 'Detail',
+      component: Detail
     }
   ]
 })
